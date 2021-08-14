ccb90fc4b83210f6c2a38a8fed971c307910f564
holequotin <69898497+holequotin@users.noreply.github.com>
2021-08-14 22:20:04 +0700

    Create binarystring2.cpp

---
+++ binarystring2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+//Sử dụng BackTracking để in ra tất cả chuỗi nhị phân độ dài n
+void Out(int n,int arr[]){
+    for(int i=1;i<=n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+void Try(int n,int arr[],int k){
+    for(int i=0;i<=1;i++){
+        arr[k]=i;
+        if(k==n) Out(n,arr);
+        else{
+            Try(n,arr,k+1);
+        }
+    }
+}
+int main() {
+int n,arr[100];
+cin>>n;
+Try(n,arr,1);
+    return 0;
+}
